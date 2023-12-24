9ac87bb1a5a7591972523331b1fb2b7b14217b99
CodeMaster7000 <CodeMaster7K@gmail.com>
2023-12-24 13:32:01 +0000

    Update main.js

--- Delete Spam Pull Requests/main.js
+++ Delete Spam Pull Requests/main.js
@@ -1,9 +1,8 @@
 const fetch = require('node-fetch');
 const readline = require('readline');
 const GITHUB_API = 'https://api.github.com';
+const accessToken = 'your_access_token'; // Replace with your GitHub access token
 
-const organization = 'your_organization'; // Replace with your GitHub organization
-const accessToken = 'your_access_token'; // Replace with your GitHub access token.
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -11,13 +10,16 @@ const rl = readline.createInterface({
 
 async function fetchJson(url, options = {}) {
   const response = await fetch(url, options);
+
   if (!response.ok) {
     throw new Error(`Failed to fetch data. Status: ${response.status}`);
   }
+
   return response.json();
 }
-async function getOpenPullRequests() {
-  const url = `${GITHUB_API}/orgs/${organization}/repos?per_page=100&type=public`;
+
+async function getOpenPullRequests(owner, repoName) {
+  const url = `${GITHUB_API}/repos/${owner}/${repoName}/pulls?state=open`;
   const options = {
     headers: {
       Authorization: `Bearer ${accessToken}`,
@@ -25,20 +27,11 @@ async function getOpenPullRequests() {
     },
   };
 
-  // Fetch repositories within the organization
-  const repositories = await fetchJson(url, options);
-  // Fetch open pull requests for each repository
-  const pullRequestsPromises = repositories.map(repo => {
-    const prUrl = `${GITHUB_API}/repos/${organization}/${repo.name}/pulls?state=open`;
-    return fetchJson(prUrl, options);
-  });
-
-  const pullRequestsArrays = await Promise.all(pullRequestsPromises);
-  return pullRequestsArrays.flat();
+  return fetchJson(url, options);
 }
 
-async function closePullRequest(prNumber) {
-  const url = `${GITHUB_API}/repos/${organization}/repoName/pulls/${prNumber}`;
+async function closePullRequest(owner, repoName, prNumber) {
+  const url = `${GITHUB_API}/repos/${owner}/${repoName}/pulls/${prNumber}`;
   const options = {
     method: 'PATCH',
     headers: {
@@ -52,18 +45,20 @@ async function closePullRequest(prNumber) {
   return fetchJson(url, options);
 }
 
-async function reviewAndClose(pr) {
-  console.log(`Pull Request #${pr.number} - Title: ${pr.title}`);
-  const answer = await askQuestion('Would you like to manually review this PR? (yes/no): ');
-  if (answer.toLowerCase() === 'yes') {
-    console.log('Manually reviewing Pull Request. Skipping closure.');
-  } else {
-    try {
-      await closePullRequest(pr.number);
-      console.log(`Closed Pull Request #${pr.number} as spam.`);
-    } catch (error) {
-      console.error(`Error closing Pull Request #${pr.number}:`, error.message);
+async function reviewAndClose(owner, repoName, pullRequest) {
+  console.log(`Pull Request #${pullRequest.number} - Title: ${pullRequest.title}`);
+
+  try {
+    const answer = await askQuestion('Would you like to manually review this Pull Request? (yes/no): ');
+
+    if (answer.toLowerCase() === 'yes') {
+      console.log('Manually reviewing Pull Request. Skipping closure.');
+    } else {
+      await closePullRequest(owner, repoName, pullRequest.number);
+      console.log(`Closed Pull Request #${pullRequest.number} as spam.`);
     }
+  } catch (error) {
+    console.error('Error during review:', error.message);
   }
 }
 
@@ -75,18 +70,17 @@ function askQuestion(question) {
   });
 }
 
-let spamPRs = [];
-
-async function closeSpamPRs() {
+async function closeSpamPullRequests() {
   try {
-    const openPullRequests = await getOpenPullRequests();
-    spamPRs = openPullRequests.filter(
+    const owner = await askQuestion('Enter the repository owner (organization or username): ');
+    const repoName = await askQuestion('Enter the repository name: ');
+    const openPullRequests = await getOpenPullRequests(owner, repoName);
+    const spamPullRequests = openPullRequests.filter(
       pr => pr.labels.some(label => label.name.toLowerCase() === 'spam')
     );
 
-    while (spamPRs.length > 0) {
-      const pr = spamPRs.shift();
-      await reviewAndClose(pr);
+    for (const pullRequest of spamPullRequests) {
+      await reviewAndClose(owner, repoName, pullRequest);
     }
 
     console.log('No more spam Pull Requests.');
@@ -97,4 +91,4 @@ async function closeSpamPRs() {
   }
 }
 
-closeSpamPRs();
+closeSpamPullRequests();
